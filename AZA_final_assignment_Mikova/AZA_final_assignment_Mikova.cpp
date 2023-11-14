daa795f1ddd37cd0cda72e86e3174fb2aaa07e22
Petra Mikova <petramikova10@gmail.com>
2023-11-14 01:56:44 +0100

    working task 1 for assignment no refactoring

--- AZA_final_assignment_Mikova/AZA_final_assignment_Mikova.cpp
+++ AZA_final_assignment_Mikova/AZA_final_assignment_Mikova.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 
+using namespace std;
+
 class Job {
 public:
     int deadline;
@@ -11,77 +13,86 @@ public:
     Job(int d, int p, int i) : deadline(d), profit(p), index(i) {}
 };
 
-bool isFeasible(const std::vector<int>& schedule, const std::vector<Job>& jobs) {
-    std::vector<int> jobScheduled(jobs.size(), 0);
-    int currentTime = 0;
+vector<int> isFeasible(const vector<int>& jobSet, const vector<Job>& sortedJobs) {
+    int numJobs = jobSet.size();
+    vector<int> schedulingResult(numJobs, -1);
+    vector<bool> timeSlotOccupied(numJobs, false);
+    Job job = Job(0, 0, 0);
 
-    for (int i : schedule) {
-        const Job& job = jobs[i - 1];
-        if (jobScheduled[i - 1] == 0) {
-            jobScheduled[i - 1] = 1;
-            currentTime += job.profit;
+    for (int jobIndex = 0; jobIndex < numJobs; jobIndex++) {
 
-            if (currentTime > job.deadline) {
-                return false;
+        for (int i = 0; i < sortedJobs.size(); ++i) {
+            if (jobSet[jobIndex] == sortedJobs[i].index){
+                job = sortedJobs[i];
             }
         }
-        else {
-            return false;
-        }
-    }
 
-    return true;
-}
-
-std::vector<int> schedule(int n, std::vector<Job>& jobs) {
-    std::vector<int> J;
-
-    for (int i = 2; i <= n; i++) {
-        std::vector<Job> K = jobs;
-        int j = 0;
-        while (j < K.size() && jobs[i].deadline < K[j].deadline)
-            j++;
-
-        K.insert(K.begin() + j, jobs[i]);
-
-        if (isFeasible(J, K)) {
-            J.clear();
-            for (const Job& job : K) {
-                J.push_back(job.index);
+        for (int slotIndex = min(numJobs, job.deadline) - 1; slotIndex >= 0; slotIndex--) {
+            if (!timeSlotOccupied[slotIndex]) {
+                schedulingResult[slotIndex] = job.index;
+                timeSlotOccupied[slotIndex] = true;
+                break;
             }
         }
     }
-
-    return J;
+    return schedulingResult;
 }
+
+vector<int> schedule(const vector<Job>& sortedJobs) {
+    vector<int> jobSet;
+
+    for (const Job& job : sortedJobs) {
+        vector<int> candidateSet = jobSet;
+        candidateSet.push_back(job.index);
+
+        vector<int> feasibleSequence = isFeasible(candidateSet, sortedJobs);
+
+        if (count(feasibleSequence.begin(), feasibleSequence.end(), -1) == 0) {
+            jobSet = feasibleSequence;
+        }
+    }
+
+    return jobSet;
+}
+
 
 int main() {
-    int n = 5;
     std::vector<Job> jobs;
-    std::vector<int> J;
 
+    /*
     jobs.push_back(Job(2, 40, 1));
     jobs.push_back(Job(4, 15, 2));
-    jobs.push_back(Job(3, 15, 3));
+    jobs.push_back(Job(3, 60, 3));
     jobs.push_back(Job(2, 20, 4));
     jobs.push_back(Job(3, 10, 5));
+    jobs.push_back(Job(1, 45, 6));
+    jobs.push_back(Job(1, 55, 7));
+    */
+
+    /*
+    jobs.push_back(Job(3, 40, 1));
+    jobs.push_back(Job(1, 35, 2));
+    jobs.push_back(Job(1, 30, 3));
+    jobs.push_back(Job(3, 25, 4));
+    jobs.push_back(Job(1, 20, 5));
+     */
 
     std::sort(jobs.begin(), jobs.end(), [](const Job& a, const Job& b) {
-        return a.deadline > b.deadline;
-        });
+        return a.profit > b.profit;
+    });
 
-    J = schedule(n, jobs);
+    std::vector<int> optimalSchedule = schedule(jobs);
 
-    if (isFeasible(J, jobs)) {
+    if (!optimalSchedule.empty()) {
         std::cout << "Optimal Schedule J: ";
-        for (int job : J) {
+        for (int job : optimalSchedule) {
             std::cout << job << " ";
         }
         std::cout << std::endl;
-    }
-    else {
-        std::cout << "The schedule is not feasible." << std::endl;
+    } else {
+        std::cout << "No optimal schedule found." << std::endl;
     }
 
     return 0;
 }
+
